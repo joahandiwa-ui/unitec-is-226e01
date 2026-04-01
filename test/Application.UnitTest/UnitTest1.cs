24ae16a98ff4b7af6e4fb2fa43bcb9cf10df1142
Gabriel <joahan.diwa@gmail.com>
2026-04-01 01:37:53 -0600

    Pruebas actualizadas del perimetro

--- test/Application.UnitTest/UnitTest1.cs
+++ test/Application.UnitTest/UnitTest1.cs
@@ -7,6 +7,24 @@
 [InlineData(2, 8, 4, 14)] // Lado 10 -> Perímetro 40
 [InlineData(3, 4, 5, 12)]
 [InlineData(5, 12, 13, 30)]
+[InlineData(10, 10, 10, 30)]
+[InlineData(6, 8, 10, 24)]
+[InlineData(7, 24, 25, 56)]
+[InlineData(8, 15, 17, 40)]
+[InlineData(9, 40, 41, 90)]
+[InlineData(11, 60, 61, 132)]
+[InlineData(12, 35, 37, 84)]
+[InlineData(20, 21, 29, 70)]
+[InlineData(2, 2, 3, 7)]
+[InlineData(5, 5, 8, 18)]
+[InlineData(10, 10, 5, 25)]
+[InlineData(1.5, 2.5, 3.5, 7.5)]
+[InlineData(0.5, 0.5, 0.5, 1.5)]
+[InlineData(100, 100, 100, 300)]
+[InlineData(15, 20, 25, 60)]
+[InlineData(1, 1, 1, 3)]
+[InlineData(50, 40, 30, 120)]
+[InlineData(9, 9, 9, 27)]
 public void CalcularPerimetroTriangulo_LadoValido_Correcto(double lado1, double lado2, double Base, double expected)
 {
 // Arrange
