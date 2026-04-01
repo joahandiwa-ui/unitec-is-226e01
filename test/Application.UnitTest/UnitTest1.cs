509553a53ba00238c87a0d10d6675c79c339463f
Yoss <yocelincarmonasoto393@gmail.com>
2026-03-31 23:08:49 -0600

    14

--- test/Application.UnitTest/UnitTest1.cs
+++ test/Application.UnitTest/UnitTest1.cs
@@ -22,6 +22,8 @@ Assert.Equal(expected, actual);
 [InlineData(6, 5, 15)] // Lado 2 -> Área 4
 [InlineData(4, 6, 12)] // Lado 5 -> Área 25
 [InlineData(2, 8, 8)] // Lado 10 -> Área 100
+[InlineData(3, 8, 12)]
+[InlineData(16, 2, 16)]
 public void CalcularAreaCuadrado_LadoValido_Correcto(double Altura, double Base, double expected)
 {
 // Arrange
@@ -38,6 +40,9 @@ Assert.Equal(expected, actual);
 [InlineData(9, 3, 27)] // Área 9, Altura 3 -> Volumen 27
 [InlineData(25, 5, 125)] // Área 25, Altura 5 -> Volumen 125
 [InlineData(23, 2, 46)]
+[InlineData(20, 3, 60)]
+[InlineData(10, 4, 40)]
+
 public void CalcularVolumenCubo_AreaYAlturaValidas_Correcto(double area, double Altura, double expected)
 {
 // Arrange
